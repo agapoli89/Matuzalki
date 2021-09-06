aa57372c1c380833a566c1fdf4b98e02cbe6e80a
Agnieszka Polińska <agapoli@gmail.com>
2021-09-06 08:14:00 +0200

    Style corrections

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -16,7 +16,8 @@ const HomePage = () => {
                 <div className="home-page__adoption"></div>
                 <h3 className="home-page__title">Adopcja...</h3>
                 <p className="home-page__text home-page__text--outstanding">...temat rzeka. O&nbsp;adopcji bezpańskich zwierząt napisano już chyba wszystko. Bezspornie jest to&nbsp;forma pomocy udzielona komuś, kto nie potrafi obronić się sam i&nbsp;jest całkowicie zależny od&nbsp;człowieka. <blockquote className="home-page__quote"><p>„Adoptując jednego psa nie zmienisz całego świata, ale&nbsp;zmienisz świat tego psa”.</p></blockquote> I&nbsp;to już wystarczająco dużo, aby walczyć o&nbsp;jednego psa, każdego po kolei.</p>
-                <strong className="home-page__text home-page__text--outstanding">KONTAKT W SPRAWIE ADOPCJI: fpz@matuzalki.pl / tel. 519 499 344 (ELA)</strong>
+                <strong className="home-page__text home-page__text--outstanding home-page__text--start">KONTAKT W SPRAWIE ADOPCJI: 
+                fpz@matuzalki.pl / tel. 519 499 344 (ELA)</strong>
                 <button className="home-page__button home-page__button--outstanding">Dowiedz się więcej</button>
             </article>
             <div className="transition"></div>
@@ -24,7 +25,7 @@ const HomePage = () => {
                 <h3>Wesprzyj nas</h3>
                 <p className="home-page__text"> Matuzalki to&nbsp;organizacja NON-PROFIT. Jeżeli chcesz i&nbsp;możesz to&nbsp;wspomóż nasze zwierzaki grosikiem przekazując darowiznę.</p>
                 <div className="home-page__support">
-                    <h3>ING BANK ŚLĄSKI:</h3>
+                    <h3 style={{margin: 0}}>ING BANK ŚLĄSKI:</h3>
                     <strong style={{textAlign: 'center'}}>57 1050 1575 1000 0090 3092 7736</strong>
                     <h4 style={{marginBottom: 4}}>Wpłaty spoza Polski (euro)</h4>
                     <strong>Kod SWIFT : INGBPLPW</strong>
