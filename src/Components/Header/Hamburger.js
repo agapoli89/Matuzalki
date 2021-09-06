d8ad5a6d1058d55721f906bc9d3422c806d97566
Agnieszka Polińska <agapoli@gmail.com>
2021-09-06 08:13:09 +0200

    Toggle menu in mobile view

--- src/Components/Header/Hamburger.js
+++ src/Components/Header/Hamburger.js
@@ -1,8 +1,8 @@
 import {IoMenu} from "react-icons/io5";
 
-const Hamburger = () => {
+const Hamburger = ({toggleMenu}) => {
     return (  
-        <button className="hamburger"><IoMenu /></button>
+        <button className="hamburger" onClick={toggleMenu}><IoMenu /></button>
     );
 }
  
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,22 @@
 import Hamburger from './Hamburger';
+import Navigation from './Navigation/Navigation';
 
 import logo from './../../images/logo2.png';
 import './Header.scss';
+import { useState } from 'react';
 
 const Header = () => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    const toggleMenu = () => setIsMenuVisible(prev => !prev);
+
     return (  
       <header className="header">
         <a>
           <img src={logo} alt="logo" className="header__logo"></img>
         </a>
-        <Hamburger />
+        <Hamburger toggleMenu={toggleMenu}/>
+        {isMenuVisible && <Navigation />}
       </header>
     );
 }
