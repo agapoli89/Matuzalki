85b1b76b88810e5ca342162bf533d094f3083e2c
Agnieszka Polińska <agapoli@gmail.com>
2021-09-21 14:06:42 +0200

    Members and VolunteersAndFriends page

--- src/Pages/Fundation/Members.js
+++ src/Pages/Fundation/Members.js
@@ -1,6 +1,59 @@
+import { IoMail } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+import Button from '../../Components/Button/Button';
+import { members } from './MembersData';
+
+import Miron from '../../images/people/Miron2.png';
+import Basia from '../../images/people/Basia2.png';
+import Ela from '../../images/people/Ela2.png';
+import Joasia from '../../images/people/Joasia.png';
+import Asia from '../../images/people/Asia2.png';
+import Magda from '../../images/people/Magda.png';
+
+import './Members.scss';
+
 const Members = () => {
+
+    const imgToDisplay = (img) => {
+        switch (img) {
+            case "Miron":
+                return Miron;
+            case "Basia":
+                return Basia;
+            case "Ela":
+                return Ela;
+            case "Joasia":
+                return Joasia;
+            case "Asia":
+                return Asia;
+            default:
+                return Magda;
+        }
+    }
+
+    const membersToDisplay = members.map(({ id, name, position, description, email, linkToPhone, phone, img, addInfoClass, addPictureClass }) => (
+        <section className="member" key={id}>
+            <div className="member__picture"><img src={imgToDisplay(img)} alt="Miron"/></div>
+            <ul className="member__contact">
+                {email && <li className="member__contact__item"><a className="member__contact__item" href={`mailto:${email}`}><IoMail title={email} /></a></li>}
+                {linkToPhone && <li className="member__contact__item"><a className="member__contact__item" href={`tel:${linkToPhone}`}><FaPhoneAlt title={phone}/></a></li>}
+            </ul>
+            <div className="member__info">
+                <h3 className="member__info__name">{name}</h3>
+                <span className="member__info__position">{position}</span>   
+            </div>
+            <p className="member__description">{description}</p>
+        </section>
+    ))
+
     return (  
-        <div>Members</div>
+        <main className="article article--justify article--medium">
+            <h2>Członkowie Fundacji</h2>
+            <div className="members-box">
+                {membersToDisplay}
+            </div>
+            <Button text={"Poznaj też naszych wolontariuszy i PSIjaciół"} link={"/wolontariusze-i-psijaciele"} addClass="center"/>
+        </main>
     );
 }
  
--- src/Pages/Fundation/VolunteersAndFriends.js
+++ src/Pages/Fundation/VolunteersAndFriends.js
@@ -1,6 +1,36 @@
+import { IoLogoFacebook, IoLogoInstagram, IoGlobeOutline} from "react-icons/io5";
+
+import Monika from '../../images/people/monika.jpg';
+import Eliza from '../../images/people/Eliza.png';
+
 const VolonteersAndFriends = () => {
     return (  
-        <div>VolonteersAndFriends</div>
+        <main className="article article--justify article--medium">
+            <h2>Nasi Wolontariusze i&nbsp;PSIjaciele - bez nich nie damy rady...</h2>
+            <div className="members-box">
+                <section className="member">
+                    <div className="member__picture"><img src={Monika} alt="Miron"/></div>
+                    <div className="member__info">
+                        <h3 className="member__info__name">Monika Świteńka</h3>
+                        <span className="member__info__position">Nasza Wolontariuszka</span>   
+                    </div>
+                    <p className="member__description">Dobra dusza Fundacji. Absolutnie wrażliwa na psi los. Ze swoją rodziną u boku pomaga jak może,... a może wiele! :)</p>
+                </section>
+                <section className="member">
+                    <div className="member__picture"><img src={Eliza} alt="Miron"/></div>
+                    <ul className="member__contact">
+                        <li className="member__contact__item"><a className="member__contact__item" href="https://www.facebook.com/zjawagold?_rdc=1&_rdr" target="_blank" rel="noreferrer"><IoLogoFacebook /></a></li>
+                        <li className="member__contact__item"><a className="member__contact__item" href="https://www.instagram.com/positivedogwroclaw/" target="_blank" rel="noreferrer"><IoLogoInstagram /></a></li>
+                        <li className="member__contact__item"><a className="member__contact__item" href="https://positivedog.pl/?fbclid=IwAR11KZOvwy8fDkJT4j4UsjmibGcya-qFp2atu-0cf2-n7Mjm0x6oQCnaWnM" target="_blank" rel="noreferrer"><IoGlobeOutline /></a></li>
+                    </ul>
+                    <div className="member__info">
+                        <h3 className="member__info__name">Eliza Polkowska-Matejko</h3>
+                        <span className="member__info__position">Nasza Opiekunka Behawioralna</span>   
+                    </div>
+                    <p className="member__description">Założycielka i instrukrorka szkolenia psów w Positive Dog.</p>
+                </section>
+            </div>
+        </main>
     );
 }
  
