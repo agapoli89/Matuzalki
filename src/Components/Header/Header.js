956a74f445efef57508d153ca59b151017124878
Agnieszka Polińska <agapoli@gmail.com>
2021-09-26 14:39:28 +0200

    Change Header view for TheyHelpedUs page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,11 @@
-import './Header.scss'
+import './Header.scss';
+import { useLocation } from 'react-router';
 
 const Header = () => {
+    const location = useLocation();
+
     return (  
-        <main className="header">
+        <main className={`header ${location.pathname === "/wsparli-nas" && "header--for-support"}`}>
             <h1 className="header__title">Fundacja pomocy zwierzętom "MATUZALKI"</h1>
             <p className="header__text">...by starość nie musiała boleć</p>
         </main>
