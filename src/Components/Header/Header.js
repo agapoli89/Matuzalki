66367c794207627d9cedee4a6c12de4112c384b8
Agnieszka Polińska <agapoli@gmail.com>
2021-09-27 18:20:11 +0200

    Change Header image for Jancio's page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,8 +4,19 @@ import { useLocation } from 'react-router';
 const Header = () => {
     const location = useLocation();
 
+    const additionalClass = (loc) => {
+        switch (loc) {
+            case "/wsparli-nas":
+                return "header--support";
+            case "/jancio":
+                return "header--jancio";
+            default:
+                return null;
+        } 
+    }
+
     return (  
-        <main className={`header ${location.pathname === "/wsparli-nas" && "header--for-support"}`}>
+        <main className={`header ${additionalClass(location.pathname)}`}>
             <h1 className="header__title">Fundacja pomocy zwierzętom "MATUZALKI"</h1>
             <p className="header__text">...by starość nie musiała boleć</p>
         </main>
