c471ed0c0655d3ddbe20983c91fdd6824d22a477
Agnieszka Polińska <agapoli@gmail.com>
2021-10-11 14:25:59 +0200

    Change button with link structure

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,10 +2,21 @@ import { Link } from 'react-router-dom';
 import './Button.scss';
 
 const Button = ({ text, link, addClass, addClass2, handleClick }) => {
-    return (  
+
+    console.log(link)
+
+    const buttonLink = <Link to={link} className="button-link">
         <button className={`button button--${addClass} button--${addClass2}`} onClick={handleClick}>
-            {link ? <Link to={link}>{text}</Link> : text}
+            {text}
         </button>
+    </Link>
+
+    return (  
+        <>
+            {!link ? (<button className={`button button--${addClass} button--${addClass2}`} onClick={handleClick}>
+            {text}
+            </button>) : buttonLink}
+        </>
     );
 }
  
